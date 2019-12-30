0e89a83b7d44385865dd8451094bd9a2a52a66de
Samuel Lim <39858145+amadeusine@users.noreply.github.com>
2019-12-29 22:19:22 -0600

    Added mut to declarations playground (Rust)

--- rust/declarations/src/main.rs
+++ rust/declarations/src/main.rs
@@ -13,5 +13,21 @@ fn main() {
     // And here's a heap-allocated string.
     let three: _ = String::from("just a little bit longer. :)");
 
-    println!("{} {} {}", ONE, TWO, three);
+    println!("{} {} {}\n", ONE, TWO, three);
+
+    // And more with `mut`
+    let range = 1 << 10;
+    let mut four: i32 = 0;
+    // Accumulation
+    for i in 1..=range {
+        if i % 2 == 0 {
+            four += i;
+        }
+    }
+    // Or something much nicer... and faster.
+    let five = (1..=range)
+                .filter(|x| x % 2 == 0)
+                .fold(0, |acc, x| acc + x);
+
+    println!("Evens summed from 1 through {} are {} (by loop) and {} (by folding).\n", range, four, five);
 }
